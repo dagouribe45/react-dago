dcdba8a05fb2e897d49d6f38811ee2f55550e4d6
jhoao700 <jhoaobrayan2001@gmail.com>
2025-06-03 09:45:27 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>npm create vite@latest</h1>
+      <h2>DAGO URIBE GARCIA</h2>
+    </>
+  )
+}
+
+export default App
